ae9dcbfb4c222f82af96c05501647abb31ed951b
ayrton chaves <116551871+Kiruake@users.noreply.github.com>
2023-05-16 09:28:22 +0200

    création bouton switch

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@ import { RouterLink, RouterView } from 'vue-router'
 import FooterPage from "./components/Footerpage.vue"
 import HeaderPage from "./components/HeaderPage.vue"
 import Premium from './components/Premium.vue';
+import BoutonSwitch from './components/BoutonSwitch.vue';
 </script>
 
 <template>
-
- <HeaderPage /><FooterPage /><Premium/>
+<BoutonSwitch/>
+ <FooterPage /><HeaderPage /><Premium/>
   
    <RouterView />
 </template>
