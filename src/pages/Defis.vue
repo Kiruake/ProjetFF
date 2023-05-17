7217979fa676b7bf79000b43ab28ad98c3e197c6
ayrton chaves <116551871+Kiruake@users.noreply.github.com>
2023-05-17 11:23:24 +0200

    création card defis 2 + couleur body

--- src/pages/Defis.vue
+++ src/pages/Defis.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import CardDefis from '@/components/CardDefis.vue';
+import CardDefis2 from '@/components/CardDefis2.vue';
 import BoutonSwitch from '@/components/BoutonSwitch.vue';
 </script>
 
 <template>
-    <div class=" flex justify-center">
+    <div class=" flex flex-col justify-center items-center gap-8">
        <CardDefis/> 
+       <CardDefis2/>
     </div>
+
 <RouterView/>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: { 
-
-    
     
     fontFamily: {
     sans: ['Montserrat', 'sans-serif'],
