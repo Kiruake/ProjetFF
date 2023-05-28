deb0f50e6437f243edbfee3a363f33f524c3685c
ayrton chaves <116551871+Kiruake@users.noreply.github.com>
2023-05-29 01:53:18 +0200

    git hub probleme?

--- src/pages/CreationEvents.vue
+++ src/pages/CreationEvents.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-
+import Footerpage from '@/components/Footerpage.vue';
+import HeaderPage from '@/components/HeaderPage.vue';
 
 </script>
+
 <template>
+
+     <HeaderPage/>
+
+
+
+      <Footerpage/>
 
 </template>
