7b14b1e54a10375b53315ea7f5c49e06b18adb6c
ayrton chaves <116551871+Kiruake@users.noreply.github.com>
2023-06-03 14:44:20 +0200

    ajout google map api

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="mt-16 h-[690px]" id="map"></div>
+</template>
+
+<script lang="ts">
+import { ref, onMounted } from 'vue';
+
+export default {
+    setup() {
+        const map = ref(null);
+
+        onMounted(() => {
+            const google = window.google;
+            const mapOptions = {
+                center: { lat: 47.510356, lng: 6.798466 }, 
+                zoom: 12, 
+            };
+            map.value = new google.maps.Map(document.getElementById('map'), mapOptions);
+        });
+
+        return {
+            map,
+        };
+    },
+};
+</script>
+
--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,5 +1,6 @@
 <template>
     <HeaderPage/>
+    <GoogleMap/>
     <Footerpage/>
 </template>
 
@@ -9,4 +10,5 @@ import PocketBase from 'pocketbase';
 import { onMounted, ref } from 'vue';
 import Footerpage from '@/components/Footerpage.vue';
 import HeaderPage from '@/components/HeaderPage.vue';
+import GoogleMap from '@/components/GoogleMap.vue';
 </script>
